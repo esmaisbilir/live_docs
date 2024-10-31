ad70e1265b78ef660d3aab50e32893f6d9232ff6
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-10-31 13:03:59 +0300

    edit Layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
-import "./globals.css"
+import "./globals.css";
 import { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-}) 
+});
 
 export const metadata: Metadata = {
-  title: 'LiveDocs',
-  description: 'Your go-to collaborative editor'
-}
+  title: "LiveDocs",
+  description: "Your go-to collaborative editor",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { 
+        variables: {
           colorPrimary: "#3371FF",
-          fontSize: '16px'
+          fontSize: "16px",
         },
       }}
     >
@@ -34,10 +39,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
   );
 }
- 
